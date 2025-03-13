248deb72fab32dc17c44e59f378535043d1dcb86
juan_manuel_R <juan.rosas17@uabc.edu.mx>
2025-03-13 09:50:45 -0700

    tu mama we

--- src/main.py
+++ src/main.py
@@ -86,10 +86,35 @@ def main(page: ft.Page):
         ]
 
     # Vistas individuales
+    RESPUESTAS_PREDETERMINADAS = [
+    "Tienes derecho a recibir asesoría legal gratuita en casos de violencia.",
+    "Puedes solicitar medidas de protección inmediatas si te encuentras en peligro.",
+    "Es importante que sepas que nadie puede obligarte a quedarte en una situación de riesgo.",
+    "Si te sientes insegura, acude a un centro de atención especializado.",
+    "Recuerda que puedes denunciar cualquier acto de violencia ante las autoridades.",
+    ]
+
+    RESPUESTAS_PREDETERMINADAS2 = [ 
+    "Es importante que recopiles cualquier evidencia que pueda respaldar tu denuncia, como mensajes, fotos o testigos.",
+    "¿En que fecha y a que hora sucedieron los hechos?",
+    "¿Quien realizo los hechos?",
+    "Hay algun partido politico involucrado",
+    "¿En donde sucedieron los hechos",
+    "Puedes especificar la ubicacion",
+    "¿Como sucedieron los hechos?"
+    ]
+
+
+    # Índice para controlar el recorrido de la lista de respuestas
+    indice_respuesta = 0
+    indice_respuesta2 = 0
+
     def vista_conoce_derechos():
-        chat_mensajes = ft.ListView(expand=True, spacing=10, auto_scroll=True)  # Lista con desplazamiento automático
+        chat_mensajes = ft.ListView(expand=True, spacing=10, auto_scroll=True)
 
         def enviar_mensaje(e):
+            nonlocal indice_respuesta  # Permite que el índice se mantenga en cada llamada
+
             if input_mensaje.value.strip():
                 # Agregar mensaje del usuario
                 chat_mensajes.controls.append(
@@ -102,14 +127,17 @@ def main(page: ft.Page):
                     )
                 )
 
-                # Simular respuesta del chatbot
-                respuesta_chatbot = f"{input_mensaje.value[::-1]}"  # Solo invierte el texto como ejemplo
+                # Simular respuesta del chatbot recorriendo la lista
+                respuesta_chatbot = RESPUESTAS_PREDETERMINADAS[indice_respuesta]
+                indice_respuesta = (indice_respuesta + 1) % len(RESPUESTAS_PREDETERMINADAS)
+
                 chat_mensajes.controls.append(
                     ft.Column(
                         [
-                            ft.Row([ft.Image(src="src/assets/flor2.png", width=30, height=30),
-                                    ft.Text("AUREL-IA",size=16,color=ft.colors.WHITE)
-                                    ]),
+                            ft.Row([
+                                ft.Image(src="src/assets/flor2.png", width=30, height=30),
+                                ft.Text("AUREL-IA", size=16, color=ft.colors.WHITE)
+                            ]),
                             ft.Container(
                                 content=ft.Text(respuesta_chatbot, size=16, color=ft.colors.BLACK),
                                 bgcolor=ft.colors.GREY_300,
@@ -121,17 +149,18 @@ def main(page: ft.Page):
                     )
                 )
 
-                input_mensaje.value = ""  # Limpiar input
+                input_mensaje.value = ""
                 input_mensaje.update()
-                chat_mensajes.update()  # Actualizar vista
+                chat_mensajes.update()
 
         # Mensaje inicial del chatbot
         chat_mensajes.controls.append(
             ft.Column(
                 [
-                    ft.Row([ft.Image(src="src/assets/flor2.png", width=30, height=30),
-                                    ft.Text("AUREL-IA",size=16,color=ft.colors.WHITE)
-                                    ]),
+                    ft.Row([
+                        ft.Image(src="src/assets/flor2.png", width=30, height=30),
+                        ft.Text("AUREL-IA", size=16, color=ft.colors.WHITE)
+                    ]),
                     ft.Container(
                         content=ft.Text("Hola, ¿en qué te puedo ayudar?", size=16, color=ft.colors.BLACK),
                         bgcolor=ft.colors.GREY_300,
@@ -143,13 +172,13 @@ def main(page: ft.Page):
             )
         )
 
-        input_mensaje = ft.TextField(hint_text="Escribe tu mensaje...",bgcolor=ft.colors.WHITE ,border_radius=30,expand=True)
+        input_mensaje = ft.TextField(hint_text="Escribe tu mensaje...", bgcolor=ft.colors.WHITE, border_radius=30, expand=True)
         boton_enviar = ft.IconButton(ft.icons.SEND, on_click=enviar_mensaje)
 
         return [
             ft.AppBar(
                 leading=ft.IconButton(ft.icons.ARROW_BACK, on_click=regresar),
-                title=ft.Text(f"En caso de bloqueo recuerda los siguientes números:{numero_aleatorio}", size=20, color=ft.colors.WHITE),
+                title=ft.Text("Conoce tus derechos", size=20, color=ft.colors.WHITE),
                 bgcolor=ft.colors.PURPLE_300
             ),
             ft.Container(
@@ -158,12 +187,13 @@ def main(page: ft.Page):
                     [
                         ft.Container(
                             content=chat_mensajes,
-                            expand=True,  # Se ajusta automáticamente
+                            expand=True,
                             padding=10,
                             border_radius=10
                         ),
-                        ft.Container(  # Input siempre visible
-                            content=ft.Row([input_mensaje, boton_enviar,ft.IconButton(icon=ft.icons.EMERGENCY,icon_color=ft.colors.BLACK,bgcolor=ft.colors.RED_400,on_click=cerrar_app)], alignment=ft.MainAxisAlignment.CENTER),
+                        ft.Container(
+                            content=ft.Row([input_mensaje, boton_enviar, ft.IconButton(icon=ft.icons.EMERGENCY, icon_color=ft.colors.BLACK, bgcolor=ft.colors.RED_400, on_click=cerrar_app)], 
+                            alignment=ft.MainAxisAlignment.CENTER),
                             padding=10
                         )
                     ],
@@ -222,9 +252,11 @@ def main(page: ft.Page):
         ]
 
     def vista_denuncia():
-        chat_mensajes = ft.ListView(expand=True, spacing=10, auto_scroll=True)  # Lista con desplazamiento automático
+        chat_mensajes = ft.ListView(expand=True, spacing=10, auto_scroll=True)
 
         def enviar_mensaje(e):
+            nonlocal indice_respuesta2  # Permite que el índice se mantenga en cada llamada
+
             if input_mensaje.value.strip():
                 # Agregar mensaje del usuario
                 chat_mensajes.controls.append(
@@ -237,14 +269,17 @@ def main(page: ft.Page):
                     )
                 )
 
-                # Simular respuesta del chatbot
-                respuesta_chatbot = f"{input_mensaje.value[::-1]}"  # Solo invierte el texto como ejemplo
+                # Simular respuesta del chatbot recorriendo la lista
+                respuesta_chatbot = RESPUESTAS_PREDETERMINADAS2[indice_respuesta2]
+                indice_respuesta2 = (indice_respuesta2 + 1) % len(RESPUESTAS_PREDETERMINADAS2)
+
                 chat_mensajes.controls.append(
                     ft.Column(
                         [
-                            ft.Row([ft.Image(src="src/assets/flor2.png", width=30, height=30),
-                                    ft.Text("AUREL-IA",size=16,color=ft.colors.WHITE)
-                                    ]),
+                            ft.Row([
+                                ft.Image(src="src/assets/flor2.png", width=30, height=30),
+                                ft.Text("AUREL-IA", size=16, color=ft.colors.WHITE)
+                            ]),
                             ft.Container(
                                 content=ft.Text(respuesta_chatbot, size=16, color=ft.colors.BLACK),
                                 bgcolor=ft.colors.GREY_300,
@@ -256,17 +291,18 @@ def main(page: ft.Page):
                     )
                 )
 
-                input_mensaje.value = ""  # Limpiar input
+                input_mensaje.value = ""
                 input_mensaje.update()
-                chat_mensajes.update()  # Actualizar vista
+                chat_mensajes.update()
 
         # Mensaje inicial del chatbot
         chat_mensajes.controls.append(
             ft.Column(
                 [
-                    ft.Row([ft.Image(src="src/assets/flor2.png", width=30, height=30),
-                                    ft.Text("AUREL-IA",size=16,color=ft.colors.WHITE)
-                                    ]),
+                    ft.Row([
+                        ft.Image(src="src/assets/flor2.png", width=30, height=30),
+                        ft.Text("AUREL-IA", size=16, color=ft.colors.WHITE)
+                    ]),
                     ft.Container(
                         content=ft.Text("¿Te encuentras en peligro inmediato?", size=16, color=ft.colors.BLACK),
                         bgcolor=ft.colors.GREY_300,
@@ -278,13 +314,13 @@ def main(page: ft.Page):
             )
         )
 
-        input_mensaje = ft.TextField(hint_text="Escribe tu mensaje...",bgcolor=ft.colors.WHITE ,border_radius=30,expand=True)
+        input_mensaje = ft.TextField(hint_text="Escribe tu mensaje...", bgcolor=ft.colors.WHITE, border_radius=30, expand=True)
         boton_enviar = ft.IconButton(ft.icons.SEND, on_click=enviar_mensaje)
 
         return [
             ft.AppBar(
                 leading=ft.IconButton(ft.icons.ARROW_BACK, on_click=regresar),
-                title=ft.Text(f"En caso de bloqueo recuerda los siguientes números:{numero_aleatorio}", size=20, color=ft.colors.WHITE),
+                title=ft.Text("Conoce tus derechos", size=20, color=ft.colors.WHITE),
                 bgcolor=ft.colors.PURPLE_300
             ),
             ft.Container(
@@ -293,12 +329,13 @@ def main(page: ft.Page):
                     [
                         ft.Container(
                             content=chat_mensajes,
-                            expand=True,  # Se ajusta automáticamente
+                            expand=True,
                             padding=10,
                             border_radius=10
                         ),
-                        ft.Container(  # Input siempre visible
-                            content=ft.Row([input_mensaje, boton_enviar,ft.IconButton(icon=ft.icons.EMERGENCY,icon_color=ft.colors.BLACK,bgcolor=ft.colors.RED_400,on_click=cerrar_app)], alignment=ft.MainAxisAlignment.CENTER),
+                        ft.Container(
+                            content=ft.Row([input_mensaje, boton_enviar, ft.IconButton(icon=ft.icons.EMERGENCY, icon_color=ft.colors.BLACK, bgcolor=ft.colors.RED_400, on_click=cerrar_app)], 
+                            alignment=ft.MainAxisAlignment.CENTER),
                             padding=10
                         )
                     ],
